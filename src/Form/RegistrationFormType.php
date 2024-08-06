149c892965d3a5da273f635480719dda93417324
Maxcastel <maxence.nord@gmail.com>
2024-08-06 21:28:20 +0200

    RegistrationFormType: add first name and name, remove agreeTerms, change password length error message

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -17,15 +17,9 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('first_name')
+            ->add('name')
             ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
-                ],
-            ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
@@ -37,7 +31,7 @@ class RegistrationFormType extends AbstractType
                     ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'minMessage' => 'Votre mot de passe doit être d\'au moins {{ limit }} characteres',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
