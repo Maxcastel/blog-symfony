5ef651f25b54d73ae818a1465a080dfa58f0be69
Maxcastel <maxence.nord@gmail.com>
2024-08-09 23:42:41 +0200

    ArticleType: add imageUrl and custom label to input fields in buildForm

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -6,15 +6,17 @@ use App\Entity\Article;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class ArticleType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title', null, ['attr' => ['placeholder' => 'Titre de l\'article']])
-            ->add('description', null, ['attr' => ['placeholder' => 'Description de l\'article']])
-            ->add('content', null, ['attr' => ['placeholder' => 'Contenu de l\'article']])
+            ->add('title', null, ['attr' => ['placeholder' => 'Titre de l\'article'], 'label' => 'Titre' ])
+            ->add('description', null, ['attr' => ['placeholder' => 'Description de l\'article'] ])
+            ->add('imageUrl', TextType::class, ['attr' => ['placeholder' => 'Url de l\'image'], 'label' => 'Url de l\'image' ])
+            ->add('content', null, ['attr' => ['placeholder' => 'Contenu de l\'article'], 'label' => 'Contenu'  ])
         ;
     }
 
