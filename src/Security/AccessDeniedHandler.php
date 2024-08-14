4a43cc29d82c7c05547a0e66a4c5b85fcb99fb8d
Maxcastel <maxence.nord@gmail.com>
2024-08-14 02:50:31 +0200

    add src/Security/AccessDeniedHandler to redirect to login instead of showing Access Denied Exception

---
+++ src/Security/AccessDeniedHandler.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+
+class AccessDeniedHandler implements AccessDeniedHandlerInterface
+{
+    private $router;
+
+    public function __construct(RouterInterface $router)
+    {
+        $this->router = $router;
+    }
+
+    public function handle(Request $request, AccessDeniedException $accessDeniedException): ?Response
+    {
+        return new RedirectResponse($this->router->generate('app_login'));
+    }
+}
