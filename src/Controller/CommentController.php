c103f6b0021bdbe5b61f10558604cde136734348
Maxcastel <maxence.nord@gmail.com>
2024-08-14 16:15:23 +0200

    CommentController: add showCommentsAdmin & validateComment & deleteComment

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -5,14 +5,59 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\CommentRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 class CommentController extends AbstractController
 {
-    #[Route('/comment', name: 'app_comment')]
-    public function index(): Response
+    private $commentRepository;
+    private $em;
+
+    public function __construct(CommentRepository $commentRepository, EntityManagerInterface $em)
     {
-        return $this->render('comment/index.html.twig', [
-            'controller_name' => 'CommentController',
+        $this->commentRepository = $commentRepository;
+        $this->em = $em;
+    }
+
+    #[Route('/admin/comments', name: 'admin_comments')]
+    #[IsGranted('ROLE_ADMIN')]
+    public function showCommentsAdmin(): Response
+    {
+        return $this->render('comment/admin/index.html.twig', [
+            'comments' => $this->commentRepository->findBy(['isValid' => false]),
         ]);
+    }
+
+    #[Route('/admin/comments/validate/{id}', name: 'admin_comment_validate')]
+    #[IsGranted('ROLE_ADMIN')]
+    public function validateComment($id): Response
+    {
+        $comment = $this->commentRepository->find($id);
+
+        if (!$comment) {
+            throw $this->createNotFoundException('Commentaire non trouvé.');
+        }
+
+        $comment->setIsValid(true);
+        $this->em->flush();
+
+        return $this->redirectToRoute('admin_comments');
+    }
+    
+    #[Route('/admin/comments/delete/{id}', name: 'admin_comment_delete')]
+    #[IsGranted('ROLE_ADMIN')]
+    public function deleteComment($id): Response
+    {
+        $comment = $this->commentRepository->find($id);
+
+        if (!$comment) {
+            throw $this->createNotFoundException('Commentaire non trouvé.');
+        }
+
+        $this->em->remove($comment);
+        $this->em->flush();
+
+        return $this->redirectToRoute('admin_comments');
     }
 }
