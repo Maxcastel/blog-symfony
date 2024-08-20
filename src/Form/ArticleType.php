f1f78b125ae0788086855ebeb8d36020582a598a
Maxcastel <maxence.nord@gmail.com>
2024-08-20 12:46:03 +0200

    ArticleType: add athor select fied (only in edit mode)

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -3,15 +3,28 @@
 namespace App\Form;
 
 use App\Entity\Article;
+use App\Entity\User;
+use App\Repository\UserRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 
+
 class ArticleType extends AbstractType
 {
+    private $userRepository;
+    
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
@@ -20,12 +33,24 @@ class ArticleType extends AbstractType
             ->add('imageUrl', TextType::class, ['attr' => ['placeholder' => 'Url de l\'image'], 'label' => 'Url de l\'image' ])
             ->add('content', HiddenType::class, ['attr' => ['class' => 'content']]);
         ;
+
+        if ($options['isEdit']) {
+            $builder
+                ->add('user', ChoiceType::class, [
+                    'choices' => $this->userRepository->findByRole('ROLE_ADMIN'),
+                    'choice_label' => function (User $user) {
+                        return $user->getFirstName() . ' ' . $user->getName();
+                    },
+                    'label' => 'Auteur',
+                ]);
+        }
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'data_class' => Article::class,
+            'isEdit' => false,
         ]);
     }
 }
